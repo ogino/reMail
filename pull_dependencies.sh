a0110d0da7d9e8045dd438041a846cabaf607e40
Gabor Cselle <gaborcselle@gmail.com>
2010-03-08 10:59:57 -0800

    Fixed Issue 1 (compile error: openssl/ssl.h not found) by copying includes into correct directory

--- pull_dependencies.sh
+++ pull_dependencies.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/sh
 # for this to work you need to have the following installed:
+# - Xcode 3.1.3 or higher: http://developer.apple.com/
 # - git: http://help.github.com/mac-git-installation/
 # - mercurial / hg: http://mercurial.berkwood.com/
-# - wget: http://www.mactricksandtips.com/2008/07/installing-wget-on-your-mac-for-terminal.html
 
 # pull three20
 cd ..
@@ -20,5 +20,8 @@ sh build-all-deps.sh `pwd`/binaries
 mkdir -p ../../mailcore/libetpan/binaries
 cp -r binaries/Developer ../../mailcore/libetpan/binaries/Developer
 
+# libetpan needs openssl / cyrus-sasl to build correctly on iPhone 3.1+
+cp -r -v binaries/Developer/Platforms/iPhoneOS.platform/Developer/SDKs/iPhoneOS3.0.sdk/Debug/include/* ../../mailcore/libetpan/build-mac/include/.
+
 # this should be it - you can now open the ReMailIPhone Xcode project
-echo "Done - you can now open the ReMailIPhone Xcode project"
+echo "Done - if you didn't see errors, you can now open the ReMailIPhone Xcode project"
